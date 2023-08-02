6adb60af9c992e9b0d71af0fb24a3ea5361e0621
SpidY21 <yashkumarroy164@gmail.com>
2023-08-03 00:41:51 +0530

    Core Java Practice

---
+++ src/Outer.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Outer {
+    public static class Inner{
+        public static void test(){
+            System.out.println("Static inner class is running");
+        }
+    }
+}
---
+++ src/Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+    static {
+        System.out.println("static1");
+    }
+    public static void main(String[] args) {
+//        System.out.println(args[0]);
+//        System.out.println(args[1]);
+//        System.out.println(args[2]);
+        System.out.println("main");
+    }
+    static {
+        System.out.println("static2");
+    }
+}
